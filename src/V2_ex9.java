5d208cdd587a910141bb68f00d6dc0e35eb23897
Javier De Las Casas <javier.delascasaschung@gmail.com>
2018-06-18 09:42:26 +0200

    Added file for date comparison (7 days intervall)

---
+++ src/V2_ex9.java
@@ -0,0 +1,9 @@
+import java.time.LocalDate;
+
+public class V2_ex9 {
+    public static void main(String[] args) {
+        LocalDate lastRunDate = LocalDate.of(2016,11,11);
+        LocalDate today = LocalDate.of(2016,11,16);
+
+    }
+}
