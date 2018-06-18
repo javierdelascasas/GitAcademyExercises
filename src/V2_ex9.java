07eedab11feb0a5cfe1bdc9669f1644ea46d3a1c
Javier De Las Casas <javier.delascasaschung@gmail.com>
2018-06-18 10:08:37 +0200

    Added method to check if 1 week has past

--- src/V2_ex9.java
+++ src/V2_ex9.java
@@ -4,6 +4,23 @@ public class V2_ex9 {
     public static void main(String[] args) {
         LocalDate lastRunDate = LocalDate.of(2016,11,11);
         LocalDate today = LocalDate.of(2016,11,16);
-
+        if(sevenDaysPass(lastRunDate, today)){
+            System.out.println("Time to run again!");
+        } else {
+            System.out.println("Not time yet");
+        }
     }
+
+    private static boolean sevenDaysPass(LocalDate lastDate, LocalDate newDate){
+        if(lastDate.isBefore(newDate)){
+            if(lastDate.plusDays(7).isBefore(newDate)){
+                return true;
+            } else{
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
 }
