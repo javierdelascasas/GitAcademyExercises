34f3d499851e5622820806d9e38c614dfb56b50d
Javier De Las Casas <javier.delascasaschung@gmail.com>
2018-06-18 11:08:55 +0200

    Replace method for 7 days for date format exampl.

--- src/V2_ex9.java
+++ src/V2_ex9.java
@@ -1,5 +1,7 @@
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.Period;
+import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 
 public class V2_ex9 {
@@ -12,15 +14,31 @@ public class V2_ex9 {
 //            System.out.println("Not time yet");
 //        }
 
-        LocalDate christmas = LocalDate.of(2018,12,24);
-        LocalDate today = LocalDate.now();
-        System.out.println("Nästa jul blir " + christmas);
-        System.out.println("Idag är det " + today);
-        Period per = Period.between(today, christmas);
-        System.out.println("Nästa jul infaller om " + per.getYears() + " år " +
-                per.getMonths() + " månader och " + per.getDays() + " dagar.");
-        long daysLeft = ChronoUnit.DAYS.between(today,christmas);
-        System.out.println("Total " + daysLeft + " dagar.");
+//        LocalDate christmas = LocalDate.of(2018,12,24);
+//        LocalDate today = LocalDate.now();
+//        System.out.println("Nästa jul blir " + christmas);
+//        System.out.println("Idag är det " + today);
+//        Period per = Period.between(today, christmas);
+//        System.out.println("Nästa jul infaller om " + per.getYears() + " år " +
+//                per.getMonths() + " månader och " + per.getDays() + " dagar.");
+//        long daysLeft = ChronoUnit.DAYS.between(today,christmas);
+//        System.out.println("Total " + daysLeft + " dagar.");
+
+        LocalDateTime now = LocalDateTime.of(2017, 9, 19, 14, 5, 0);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd HHmm");
+        System.out.println(now.format(formatter));
+        formatter = DateTimeFormatter.ofPattern("E-dd, yyyy");
+        System.out.println(now.format(formatter));
+        formatter = DateTimeFormatter.ofPattern("EEEE-dd, yyyy");
+        System.out.println(now.format(formatter));
+        formatter = DateTimeFormatter.ofPattern("dd-LLLL, yyyy");
+        System.out.println(now.format(formatter));
+        formatter = DateTimeFormatter.ofPattern("hh:mm a");
+        System.out.println(now.format(formatter));
+        formatter = DateTimeFormatter.ofPattern("yyyy/dd/m");
+        System.out.println(now.format(formatter));
+        formatter = DateTimeFormatter.ofPattern("yyyy/dd/mm");
+        System.out.println(now.format(formatter));
 
 
     }
