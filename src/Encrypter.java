6af2f5b744e4c37c1e98968288bf52b02cdc9112
Javier De Las Casas <javier.delascasaschung@gmail.com>
2018-06-19 11:13:29 +0200

    Split the encrypter method into smaller methods

--- src/Encrypter.java
+++ src/Encrypter.java
@@ -7,14 +7,10 @@ public class Encrypter {
         for (int i = 0; i < input.length(); i++) {
             int inValue = getLetterPosition(input.charAt(i));
             int outValue = inValue + myKey[keyPosition];
-            if(outValue>S.length()-1){
-                outValue = outValue-S.length();
-            }
+            outValue = normalizeIndex(outValue);
             encrypted += getLetter(outValue);
             keyPosition++;
-            if(keyPosition>=myKey.length){
-                keyPosition = keyPosition-myKey.length;
-            }
+            keyPosition = normalizeKeyIndex(keyPosition,myKey.length);
         }
         return encrypted;
     }
@@ -25,17 +21,15 @@ public class Encrypter {
         for (int i = 0; i < input.length(); i++) {
             int inValue = getLetterPosition(input.charAt(i));
             int outValue = inValue - myKey[keyPosition];
-            if(outValue<0){
-                outValue = outValue + (S.length());
-            }
+            outValue = normalizeIndex(outValue);
             decrypted += getLetter(outValue);
             keyPosition++;
-            if(keyPosition>=myKey.length){
-                keyPosition = keyPosition-myKey.length;
-            }
+            keyPosition = normalizeKeyIndex(keyPosition,myKey.length);
         }
         return decrypted;
     }
+
+    // Convert the our key to an int array
     private static int[] convertKey(String key){
         int[] myKey = new int[key.length()];
         for (int i = 0; i < key.length(); i++) {
@@ -43,14 +37,37 @@ public class Encrypter {
         }
         return myKey;
     }
+    // Gets the index of our letter
     private static int getLetterPosition(char c){
             String all = S;
             int index = all.indexOf(c);
             return index;
     }
+    // Gets our letter from an index
     private static char getLetter(int i){
             String all = S;
             char c = all.charAt(i);
             return c;
     }
+    // Normalize letter index
+    private static int normalizeIndex(int index){
+        if(index>S.length()-1){
+            index = index-S.length();
+        }
+        if(index<0){
+            index = index + (S.length());
+        }
+        return index;
+    }
+    // Normalize key index
+    private static int normalizeKeyIndex(int index, int length){
+        if(index>=length){
+            index = index-length;
+        }
+        if(index>=length){
+            index = index-length;
+        }
+        return index;
+    }
+
 }
