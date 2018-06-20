82ecbd370d2a8cc0b1f953919eaf7bc097b7556c
Javier De Las Casas <javier.delascasaschung@gmail.com>
2018-06-20 13:25:08 +0200

    Refactored out Bank class to Logging

--- src/Bank.java
+++ src/Bank.java
@@ -9,21 +9,12 @@ public class Bank {
     public void sendMoney(int amount, String fromAccount, String toAccount) throws
             IOException {
         String logstr = "";
+
 //Calculate the transfer cost
-        int paymentCost = 10;
-        if (amount > 1000)
-            paymentCost = 50;
+        int paymentCost = setPaymentCost(amount);
+
 //Write to a log file for debugging purposes
-        logstr = "Bank auditlog" + "\r\n";
-        Files.write(Paths.get("./log.txt"), logstr.getBytes(),
-                StandardOpenOption.CREATE);
-        logstr = "Doing a desposit of " + amount + " Sek from " + fromAccount + " to "
-                + toAccount + "\r\n";
-        Files.write(Paths.get("./log.txt"), logstr.getBytes(),
-                StandardOpenOption.APPEND);
-        logstr = "Payment cost" + paymentCost + "\r\n";
-        Files.write(Paths.get("./log.txt"), logstr.getBytes(),
-                StandardOpenOption.APPEND);
+        Logging.createLog(amount,fromAccount,toAccount,paymentCost);
         System.out.printf("Payment cost " + paymentCost);
 //save a receipt to disk
         String receipt = "Bank transfer receipt\r\n" +
@@ -35,5 +26,13 @@ public class Bank {
         Files.write(Paths.get("./confirmation.txt"), receipt.getBytes(),
                 StandardOpenOption.CREATE);
 //Doing the bank transfer somehow....
+    }
+
+    private int setPaymentCost(int amount){
+        int paymentCost = 10;
+        if(amount > 1000){
+            paymentCost = 50;
+        }
+        return paymentCost;
     }
 }
