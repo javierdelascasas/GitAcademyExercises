1877726cd96832c2d20d8edbe617b53da9aca1a0
Javier De Las Casas <javier.delascasaschung@gmail.com>
2018-06-20 13:23:14 +0200

    Refactored Logging class even further

--- src/Logging.java
+++ src/Logging.java
@@ -1,13 +1,17 @@
-
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
 public class Logging {
-    private String logstr;
-    private int amount;
-    private String fromAccount;
-    private String toAccount;
-    private int paymentCost;
+//    private String logstr;
+//    private int amount;
+//    private String fromAccount;
+//    private String toAccount;
+//    private int paymentCost;
 
-    public Logging() {
+    public static void createLog(int amount,String fromAccount,String toAccount,int paymentCost) throws IOException {
+        String logstr;
         logstr = "Bank auditlog" + "\r\n";
         startLogEntry(logstr);
         logstr = "Doing a desposit of " + amount + " Sek from " + fromAccount + " to "
@@ -15,6 +19,15 @@ public class Logging {
         appendLogEntry(logstr);
         logstr = "Payment cost" + paymentCost + "\r\n";
         appendLogEntry(logstr);
-        System.out.printf("Payment cost " + paymentCost);
+    }
+
+    private static void startLogEntry(String logstr) throws IOException {
+        Files.write(Paths.get("./log.txt"), logstr.getBytes(),
+                StandardOpenOption.CREATE);
+    }
+
+    private static void appendLogEntry(String logstr) throws IOException{
+        Files.write(Paths.get("./log.txt"), logstr.getBytes(),
+                StandardOpenOption.APPEND);
     }
 }
