21d16dc2dbf0acf267ae55c444c4811cf52ada1f
Javier De Las Casas <javier.delascasaschung@gmail.com>
2018-06-20 14:08:07 +0200

    Changed copy constructor in Car class

--- src/Car.java
+++ src/Car.java
@@ -9,6 +9,18 @@ public class Car {
         this.doors = doors;
     }
 
+    public Car(Car car){
+        Tire[] wheels = new Tire[car.wheels.length];
+        for (int i = 0; i < car.wheels.length; i++) {
+            wheels[i] = new Tire(car.wheels[i].getWeight());
+        }
+        Door[] doors = new Door[car.doors.length];
+        for (int i = 0; i < car.doors.length; i++) {
+            doors[i] = new Door(car.doors[i].getWeight());
+        }
+        new Car(car.getChassiWeight(),wheels,doors);
+    }
+
     public float getWeight() {
         float totalWeight = this.weight;
         for(Tire tire:this.wheels){
@@ -18,5 +30,9 @@ public class Car {
             totalWeight += door.getWeight();
         }
         return totalWeight;
+    }
+
+    public float getChassiWeight(){
+        return weight;
     }
 }
