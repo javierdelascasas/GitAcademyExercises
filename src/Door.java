10a3553ce98bc08623551f167a39e1b6eed65280
Javier De Las Casas <javier.delascasaschung@gmail.com>
2018-06-20 13:59:48 +0200

    Added overload constructor in Door class - Float

--- src/Door.java
+++ src/Door.java
@@ -5,6 +5,8 @@ public class Door {
         this.weight = 150;
     }
 
+    public Door(float weight){  this.weight = weight;   }
+
     public Door(Door door){ this.weight = door.weight;  }
 
     public float getWeight(){
